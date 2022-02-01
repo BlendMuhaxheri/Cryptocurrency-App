ca2a86faa6460971f39336f57cf9a228dafdad73
blend <blend11muhaxheri@gmail.com>
2022-02-01 16:17:33 +0100

    fixed the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link, Switch } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import { NavBar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components/index/index'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>
+        <NavBar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path='/' component={Homepage} />
+              <Route exact path='/exchanges' component={Exchanges} />
+              <Route exact path='/cryptocurrencies' component={Cryptocurrencies} />
+              <Route exact path='/cryptodetails/:id' component={CryptoDetails} />
+              <Route exact path='/news' component={News} />
+            </Switch>
+          </div>
+        </Layout>
+
+        <div className='footer' >
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse  <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/index/index.js
@@ -0,0 +1,6 @@
+export { default as NavBar } from '../NavBar/NavBar'
+export { default as Homepage } from '../Homepage/Homepage'
+export { default as News } from '../News/News'
+export { default as CryptoDetails } from '../CryptoDetails/CryptoDetails'
+export { default as Cryptocurrencies } from '../Cryptocurrencies/Cryptocurrencies'
+export { default as Exchanges } from '../Exchanges/Exchanges'
